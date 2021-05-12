6c3816344efc7e2b6a7c969206bc67f86857c862
Kenny Boyer <ksboyerii@gmail.com>
2021-05-11 19:29:49 -0500

    working on get routes

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -1,6 +1,44 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// router.get('/profile', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: User }],
+//       });
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('profile', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+//   router.get('/shelter', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Project }],
+//       });
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('profile', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
 router.post('/login', async (req, res) => {
     try {
     const userData = await User.findOne({ where: { email: req.body.email } });
