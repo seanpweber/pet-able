c78906ba4e305c8636fb024a8c9098064a22da77
Kenny Boyer <ksboyerii@gmail.com>
2021-05-25 17:59:08 -0500

    fixed issue with PORT in server.js

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.use('/', (req, res) => {
 });
 
 sequelize.sync({force: false}).then(() => {
-    app.listen(7000, () => {
-    console.log('The web server has started on port 7000');
+    app.listen(PORT, () => {
+    console.log('The web server has started on port' + PORT);
 })
 })
 
